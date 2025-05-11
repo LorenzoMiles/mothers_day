ab1811ef4ab6f5dbdef061bffd2e96edf19f4ca0
LorenzoMiles <lorenzomiles1122@gmail.com>
2025-05-11 01:00:42 -0500

    Fix layout override so Tailwind styles apply

--- lib/mothers_day_web/controllers/page_controller.ex
+++ lib/mothers_day_web/controllers/page_controller.ex
@@ -7,7 +7,7 @@ defmodule MothersDayWeb.PageController do
     render(conn, :home, layout: false)
   end
 
-  def letter1(conn, _params), do: render(conn, :letter1)
-  def letter2(conn, _params), do: render(conn, :letter2)
-  def letter3(conn, _params), do: render(conn, :letter3)
+  def letter1(conn, _params), do: render(conn, :letter1, layout: false)
+  def letter2(conn, _params), do: render(conn, :letter2, layout: false)
+  def letter3(conn, _params), do: render(conn, :letter3, layout: false)
 end
